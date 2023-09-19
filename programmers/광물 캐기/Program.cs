8082036f295f8748157e928c63766da015e73948
beolggoolosori <ilbora@naver.com>
2023-09-19 09:10:51 +0900

    feat: 광물 캐기

---
+++ programmers/광물 캐기/Program.cs
@@ -0,0 +1,15 @@
+﻿// https://school.programmers.co.kr/learn/courses/30/lessons/172927
+namespace 광물캐기
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public static int[] solution(string today, string[] terms, string[] privacies)
+        {
+
+        }
+    }
+}
