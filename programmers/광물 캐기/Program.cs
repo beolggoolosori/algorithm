0f55cc6feb1e801d7eb3f50c41cc367158fe716d
beolggoolosori <ilbora@naver.com>
2023-09-19 09:15:50 +0900

    fix: 테스트케이스 추가.

--- programmers/광물 캐기/Program.cs
+++ programmers/광물 캐기/Program.cs
@@ -5,9 +5,15 @@ namespace 광물캐기
     {
         static void Main(string[] args)
         {
+            int[] picks1 = { 1, 3, 2 };
+            string[] minerals1 = { "diamond", "diamond", "diamond", "iron", "iron", "diamond", "iron", "stone" };
+            Console.WriteLine($"Expected: 12, Result: {Solution(picks1, minerals1)}");
 
+            int[] picks2 = { 0, 1, 1 };
+            string[] minerals2 = { "diamond", "diamond", "diamond", "diamond", "diamond", "iron", "iron", "iron", "iron", "iron", "diamond" };
+            Console.WriteLine($"Expected: 50, Result: {Solution(picks2, minerals2)}");
         }
-        public static int[] solution(string today, string[] terms, string[] privacies)
+        public static int Solution(int[] picks, string[] minerals)
         {
 
         }
